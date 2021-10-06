621f6e1f0bbd1ef01e8db595c5de36bb1b7bf0c6
ampo0809 <ampuero0809@gmail.com>
2021-10-06 18:04:47 +0200

    Added 2 classes and the dealing system

--- hilo/game/dealer.py
+++ hilo/game/dealer.py
@@ -1,6 +1,47 @@
+from random import choice
 from game.player import Player
 
 class Dealer:
+
+    def __init__(self):
+        self.player = Player()
+        self.keep_playing = True
     
     def start_dealing(self):
+        # For fun
         print("Deal...")
+
+        # Self explanatory
+        self.player.generate_cards()
+        self.present_points()
+
+        while self.keep_playing:
+            self.next_card()
+
+    def present_points(self):
+        print(f"Your current points: {self.player.points}")
+
+    def next_card(self):
+        cards = self.player.card_set
+        print(f"The card is: {cards[0]}")
+
+        choice = input("Higher or lower? [h/l] ")
+
+        # TODO the whole point system needs to be built
+
+        if choice == "h":
+            # Add points
+            print("+ points")
+
+        elif choice == "l":
+            # Subtract points
+            print("- points")
+
+        else:
+            print("Sorry, that's not an option. GAME OVER!")
+            quit()
+
+        # Remove the used card from the list and checks if there are still cards to play
+        cards.pop(0)
+        if len(cards) == 0:
+            self.keep_playing = False
--- hilo/game/player.py
+++ hilo/game/player.py
@@ -1,2 +1,20 @@
+import random
+
 class Player:
-    pass 
+    
+    def __init__(self):
+        self.points = 300
+        self.card_set = []
+
+    def generate_cards(self):
+        # Populates `self.card_set` with 7 new cards
+        for i in range(7):
+            card = random.randint(1, 13)
+            self.card_set.append(card)
+
+        # In case of wanting to cheat, uncomment that below
+        # print(self.card_set)
+    
+    def calculate_points(self):
+        # TODO
+        pass
