2ad1b25e897cf5732641ac597d5750fb06b7c7b7
Hyperdriveguy <hyperdriveguy@gmail.com>
2021-10-07 13:55:09 -0600

    add skeleton classes

---
+++ hilo/game/card.py
@@ -0,0 +1,13 @@
+import random
+
+class Card:
+
+    def __init__(self):
+        self.last_card = 0
+
+    def draw_card(self):
+        pass
+
+    def check_lo_hi(self):
+        pass
+        # return true or false
---
+++ hilo/game/interface.py
@@ -0,0 +1,18 @@
+
+
+class Interface:
+
+    def __init__(self):
+        pass
+
+    def start_game(self):
+        pass
+
+    def choose_hi_lo(self):
+        pass
+
+    def check_quit_game(self):
+        pass
+
+    def print_score(self):
+        pass
