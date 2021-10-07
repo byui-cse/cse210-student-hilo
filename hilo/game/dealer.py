55de2cc71f5485570a40b716543c95e95af4dfdc
samanthastaheli <sta18011@byui.edu>
2021-10-07 16:10:36 -0600

    added colors

--- hilo/game/dealer.py
+++ hilo/game/dealer.py
@@ -1,5 +1,6 @@
+import sys
+from termcolor import colored, cprint
 from random import randint
-
 class Dealer():
     def __init__(self):
         self.card_active = 0
@@ -16,27 +17,26 @@ class Dealer():
         Keeps track of the number of points.
         """
         self.points = 0
-        print(self.card_active)
-        print(self.card_next)
 
-        
-        print(f'choice = {self.choice}, active = {self.card_active}, next = {self.card_next}\n')
+        print_red = lambda x: cprint(x, 'red')
+        print_green = lambda x: cprint(x, 'green')
+        print_blue = lambda x: cprint(x, 'blue')
 
         if self.card_active < self.card_next and self.choice.lower() == "l": # sign was >
             self.points -= 75
-            print("You were incorrect!")
+            print_red("You were incorrect!")
         elif self.card_active > self.card_next and self.choice.lower() == "h": # sign was <
             self.points -= 75
-            print("You were incorrect!")
+            print_red("You were incorrect!")
         elif self.card_active > self.card_next and self.choice.lower() == "l": # sign was <
             self.points += 100
-            print("You were correct!")
+            print_green("You were correct!")
         elif self.card_active < self.card_next and self.choice.lower() == "h": # sign was >
             self.points += 100
-            print("You were correct!")
+            print_green("You were correct!")
         else:
             self.points += 0
-            print("The cards were the same.")
+            print_blue("The cards were the same.")
         return self.points
 
     def get_card(self):
