be7ba18c1b35fd21612ad0fa24a105d0eb586089
Seth Wilding <sawilding7@gmail.com>
2021-10-07 14:19:28 -0600

    changes

--- hilo/game/dealer.py
+++ hilo/game/dealer.py
@@ -2,10 +2,10 @@ from random import randint
 
 class Dealer():
     def __init__(self):
-        self.card_num = 0
+        self.card_active = 0
         self.points = 300
         self.choice = ""
-
+        self.card_next = 0
     def get_user_choice(self):
         self.choice = input("Higher or lower? [h or l] ")  
 
@@ -27,4 +27,5 @@ class Dealer():
         """
         Generates a random number between 1 and 13 for the card number.
         """
-        self.card_num = randint(1,13)
+        self.card_active = randint(1,13)
+        self.card_next = randint(1,13)
