606bab7c22e0feadc40842da4740ae64c084a94f
kyeguy00 <91562786+kyeguy00@users.noreply.github.com>
2021-10-08 01:06:49 -0600

    fixed it :)

--- hilo/__main__.py
+++ hilo/__main__.py
@@ -1,1 +1,7 @@
 # TODO: Add entry point code here
+
+from game.hilo_directory import Directory
+
+
+directory = Directory()
+directory.start_game()
---
+++ hilo/game/card.py
@@ -0,0 +1,44 @@
+import random       
+    
+class Card():
+
+    def __init__(self):
+        self.display = random.randint(1,13)
+        self.guess_card = self.display
+        #self.score = 300
+        while self.display == self.guess_card:
+            self.guess_card = random.randint(1,13)
+        '''self.displayed_card = random.randint(1,13)
+        self.next_displayed_card = random.randint(1,13)'''
+
+    
+
+    def guess_high(self):
+        return self.guess_card > self.display
+    '''def next_guess_high(self):
+        return self.next_displayed_card > self.displayed_card'''
+
+    def guess_low(self):
+        return self.guess_card < self.display
+    '''def next_guess_low(self):
+        return self.next_displayed_card < self.displayed_card'''
+        
+    """def add_points(self):
+        return  100
+    def sub_points(self):
+        return  75"""
+    
+        
+        
+
+
+
+
+
+
+
+
+
+
+
+
