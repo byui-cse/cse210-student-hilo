41c9b3d4441f0da21216728396a8ed4d53af9ae8
j-rubio <esc18010@byui.edu>
2021-10-09 12:57:04 -0700

    dealer finished

--- hilo/__main__.py
+++ hilo/__main__.py
@@ -3,6 +3,5 @@ from game.director import Director
 This calls and stores the code from the Director file into a variable
 and then runs the code to start the game.
 """
-
 director = Director()
 director.start_game()
