2b4e6e3d85e10fa2f4e487e6968da0169ae47ddd
IsaiasZc <78005126+IsaiasZc@users.noreply.github.com>
2021-10-09 13:26:43 -0500

    "The code without comments"

--- hilo/game/dealer.py
+++ hilo/game/dealer.py
@@ -1,1 +1,56 @@
 from game.gambler import Gambler
+
+class Dealer:
+    """A part of the code for the person that deals the cards
+    The responsibility of the dealer is to give out cards and 
+    to keep track of the score and call out the end of the game
+    
+    Atr.
+    score 
+    """
+
+
+    def __init__(self):
+        """The class constructor."""
+        self.score = 300
+        self.keep_playing = True
+        self.gambler = Gambler()
+        self.choice = ""
+    
+    def start_game(self):
+        """this method leads the game start each loop"""
+        while self.keep_playing:
+            self.initial_input()
+            self.update_score()
+            self.final_input()
+
+    def initial_input(self):
+        """"""
+        print(f"The card is: {self.gambler.old_card}")
+        h_o_l = input("Higher or lower? [h/l] ")
+        self.choice = h_o_l
+
+
+    def update_score(self):
+        """update the gambler score"""
+        if self.gambler.get_points(self.choice):
+            self.score += 100
+        else:
+            self.score -= 75
+
+    def final_input(self):
+        print(f"Next card was: {self.gambler.new_card}")
+        if self.score <= 0:
+            print("your score is: 0")
+            self.keep_playing = False
+        
+        if self.keep_playing:
+            print(f"Your score is: {self.score}")
+            deal = input("Keep playing? [y/n] ")
+            print()
+            self.keep_playing = (deal == "y")
+        else:
+            self.keep_playing = False
+
+        
+
--- hilo/game/gambler.py
+++ hilo/game/gambler.py
@@ -1,1 +1,26 @@
 import random
+
+class Gambler:
+    """The player."""
+
+    def __init__(self):
+        """The class constructor."""
+        self.new_card = random.randint(1,13)
+        self.old_card = self.new_card
+    
+    def get_points(self,choice):
+        self.sort_card()
+        if self.new_card > self.old_card:
+            point = "h"
+        else:
+            point = "l"
+        self.old_card = self.new_card
+        return (choice == point)
+        
+
+    def sort_card(self):
+        self.new_card = random.randint(1,13)
+        while self.new_card == self.old_card:
+            self.new_card = random.randint(1,13)
+
+    
