dbdeec928edf038b8654e494ca8b0a6c7e2900ce
Tyson Freeze <tyson.freeze@icloud.com>
2021-10-09 17:03:57 -0700

    add classes and method stubs

---
+++ hilo/game/dealer.py
@@ -0,0 +1,16 @@
+
+
+class Dealer:
+
+    
+    #eber
+    def __init__(self):
+        pass
+
+    #tyson
+    def deal_first_card(self):
+        pass
+
+    #tyson
+    def deal_second_card(self):
+        pass
