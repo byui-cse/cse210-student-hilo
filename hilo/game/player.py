19d6d95ff98787b8edaff8c3fccf2ad67dc12c3d
herrmeyerL <logern5@gmail.com>
2021-10-09 19:44:30 -0600

    Fix scoring error

--- hilo/game/player.py
+++ hilo/game/player.py
@@ -13,6 +13,7 @@ class Player:
     self.newcard = 1 # Newly drawn card
     self.oldcard = 1 # Last card (to determine if player's guess was right)
     self.is_done = False # Does the player want to be done?
+    self.point_total = 300 # Point total
     pass
   def draw_card(self):
     """Draw a card.
@@ -35,11 +36,14 @@ class Player:
       self.points += 100
     else:
       self.points -= 75
+    # Keep track of point total to see if we can play or not.
+    self.point_total += self.points
     return self.points
+    
   def can_play(self):
     """Can the player play (points > 0)?
 
     Args:
       self (Player): an instance of Player.
     """
-    return ((self.points > 0) or self.is_done)
+    return ((self.point_total > 0) or self.is_done)
