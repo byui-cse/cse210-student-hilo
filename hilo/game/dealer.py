655b451a61b463646caaa3197abfcfd21b93f301
ampo0809 <ampuero0809@gmail.com>
2021-10-11 17:22:26 +0200

    Added point system

--- hilo/game/dealer.py
+++ hilo/game/dealer.py
@@ -6,6 +6,7 @@ class Dealer:
     def __init__(self):
         self.player = Player()
         self.keep_playing = True
+        self.cards = self.player.card_set
     
     def start_dealing(self):
         # For fun
@@ -13,35 +14,41 @@ class Dealer:
 
         # Self explanatory
         self.player.generate_cards()
-        self.present_points()
 
         while self.keep_playing:
-            self.next_card()
+            self.next_card(self.cards)
+            self.present_points(self.cards)
 
-    def present_points(self):
-        print(f"Your current points: {self.player.points}")
-
-    def next_card(self):
-        cards = self.player.card_set
+    def next_card(self, cards):
+        # cards = self.player.card_set
         print(f"The card is: {cards[0]}")
 
         choice = input("Higher or lower? [h/l] ")
-
-        # TODO the whole point system needs to be built
-
-        if choice == "h":
-            # Add points
-            print("+ points")
-
-        elif choice == "l":
-            # Subtract points
-            print("- points")
-
-        else:
-            print("Sorry, that's not an option. GAME OVER!")
-            quit()
+        self.player.calculate_points(choice)
 
         # Remove the used card from the list and checks if there are still cards to play
         cards.pop(0)
-        if len(cards) == 0:
+        
+    def present_points(self, cards):
+        print(f"The card was: {cards[0]}")
+
+        points = self.player.points
+        print(f"Your score is: {points}")
+
+        play = input("Keep playing? [y/n] ")
+
+        # Act on the dession to keep playing or not, and check if it's possible to do so
+        if len(cards) == 0 or points <= 0 or play == "n":
+            self.keep_playing = False
+
+        elif play == "y":
+            self.keep_playing = True
+
+        # In case of a typo
+        elif play != "y" and play != "n":
+            print(f"Sorry, '{play}' is not an option. GAME OVER!")
+            quit()
+
+        # Just to be sure it is exhaustive
+        else:
             self.keep_playing = False
--- hilo/game/player.py
+++ hilo/game/player.py
@@ -13,8 +13,19 @@ class Player:
             self.card_set.append(card)
 
         # In case of wanting to cheat, uncomment that below
-        # print(self.card_set)
+        print(self.card_set)
     
-    def calculate_points(self):
-        # TODO
-        pass
+    def calculate_points(self, choice):
+
+        # Add points
+        if (choice == "h" and self.card_set[1] >= self.card_set[0]) or (choice == "l" and self.card_set[1] <= self.card_set[0]): 
+            self.points += 100
+
+        # Subtract points
+        elif choice == "l" or choice == "h":
+            self.points -= 75
+
+        # In case of a typo
+        else:
+            print(f"Sorry, '{choice}' is not an option. GAME OVER!")
+            quit()
