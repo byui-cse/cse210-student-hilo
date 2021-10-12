658d3cceee5f6e8fdc459cf0a87334146d9e6374
ampo0809 <ampuero0809@gmail.com>
2021-10-12 14:45:36 +0200

    Fixed testing errors. Added feature to not repeat cards

--- hilo/game/player.py
+++ hilo/game/player.py
@@ -7,18 +7,30 @@ class Player:
         self.card_set = []
 
     def generate_cards(self):
-        # Populates `self.card_set` with 7 new cards
+        
+        # Makes a list form 1 to 13 to choose from
+        card_range = list(range(1, 14))
+        
         for i in range(7):
-            card = random.randint(10, 13)
+            # Checks if the `self.card_set` to avoid filtering it
+            try:
+                n = self.card_set[-1]
+            except IndexError:
+                n = 0
+
+            # Filters the last card or the array to avoid having the same number twice in a row
+            filtered_card_range = list(filter(lambda exc: exc != n, card_range))
+
+            # Populates `self.card_set` with 7 new cards
+            card = random.choice(filtered_card_range)
             self.card_set.append(card)
 
         # In case of wanting to cheat, uncomment that below
-        print(self.card_set)
+        # print(self.card_set)
     
     def calculate_points(self, choice):
 
         # Add points
-        # Gives freeby points if the next card is the same as the previous
         if (choice == "h" and self.card_set[1] >= self.card_set[0]) or (choice == "l" and self.card_set[1] <= self.card_set[0]):
             self.points += 100
 
