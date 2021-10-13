3204e8836494e7802a28f7673b718b3fb2d487d9
Rafael Alcantara <80664084+faelalcantara@users.noreply.github.com>
2021-10-13 06:25:46 -0300

    Game is working

--- hilo/game/dealer.py
+++ hilo/game/dealer.py
@@ -3,43 +3,74 @@ from typing import Match
 from game.player import Player
 
 class Dealer:
+    """A code template for a person who directs the game. The responsibility of 
+    this class of objects is to keep track of the score and control the 
+    sequence of play.
+    
+    Attributes:
+        keep_playing (boolean): Whether or not the player wants to keep playing.
+        player (Player): An instance of the class of objects known as Player.
+    """
 
     def __init__(self):
-        self.player = Player()
+        """Class constructor. Declares and initializes instance attributes.
+
+        Args:
+            self (Dealer): An instance of Dealer.
+        """
         self.keep_playing = True
-        self.cards = self.player.card_set
+        self.player = Player()
     
     def start_dealing(self):
         # For fun
+        print("\n------ Welcome to Hilo Game ------")
         print("Deal...")
 
         # Self explanatory
-        self.player.generate_cards()
+        card = self.player.generate_card()
+        self.player.populate_cards_set(card)
 
         while self.keep_playing:
-            self.current_card = self.enhance_cards(self.cards)
-            self.next_card(self.current_card)
-            self.present_points(self.current_card)
+            self.next_card()
 
-    def next_card(self, current_card):
-        print(f"The card is: {current_card}")
+    def next_card(self):
+        # Control the game giving the next card and printing important info about it.
+        current_card = self.player.generate_card()
+        last_card = self.player.get_last_card()
+        print(f"\nThe card is: {last_card}")
 
         choice = input("Higher or lower? [h/l] ")
-        self.player.calculate_points(choice)
+        answer = ''
 
-        # Remove the used card from the list and checks if there are still cards to play
-        self.cards.pop(0)
+        if current_card > last_card:
+            answer = 'h'
+        elif current_card < last_card:
+            answer = 'l'
         
-    def present_points(self, current_card):
+        # Compute score based on player's answer
+        if choice.lower() == 'h' or choice.lower() == 'l':
+            if choice.lower() == answer:
+                self.player.compute_points('add')
+            else:
+                self.player.compute_points('remove')
+        else:
+            print(f"Sorry, '{choice}' is not an option. GAME OVER!")
+            quit()
+
         print(f"The card was: {current_card}")
 
+        self.player.populate_cards_set(current_card)
+        self.present_points()
+      
+    def present_points(self):
+        # Display the points and verify if the player want to keep playing.
         points = self.player.points
         print(f"Your score is: {points}")
 
         play = input("Keep playing? [y/n] ")
 
-        # Act on the dession to keep playing or not, and check if it's possible to do so
-        if len(self.cards) == 0 or points <= 0 or play == "n":
+        # Act on the dession to keep playing or not, and check if it's possible to do so.
+        if len(self.player.card_set) == 0 or points <= 0 or play == "n":
             self.keep_playing = False
 
         elif play == "y":
@@ -48,23 +79,8 @@ class Dealer:
         # In case of a typo
         elif play != "y" and play != "n":
             print(f"Sorry, '{play}' is not an option. GAME OVER!")
-            quit()
+            self.keep_playing = False
 
         # Just to be sure it is exhaustive
         else:
             self.keep_playing = False
-
-    def enhance_cards(self, cards):
-
-        current_card = cards[0]
-        # Simple enhancer, could be commented out without affecting anything else
-        if current_card == 1:
-            current_card = "A"
-        elif current_card == 11:
-            current_card = "J"
-        elif current_card == 12:
-            current_card = "Q"
-        elif current_card == 13:
-            current_card = "K"
-
-        return current_card
--- hilo/game/player.py
+++ hilo/game/player.py
@@ -1,44 +1,74 @@
 import random
 
 class Player:
+    """A code template for a person who plays the game. The responsibility of this 
+    class of objects is to play the game, keep track of the cards and the score.
+    
+    Attributes:
+        points: The current score in the game.
+        card_set: The list of cards being played.
+    """
     
     def __init__(self):
+        """Class constructor. Declares and initializes instance attributes.
+
+        Args:
+            self (Player): An instance of Player.
+        """
         self.points = 300
         self.card_set = []
 
-    def generate_cards(self):
-        
-        # Makes a list form 1 to 13 to choose from
-        card_range = list(range(1, 14))
-        
-        for i in range(7):
-            # Checks if the `self.card_set` to avoid filtering it
-            try:
-                n = self.card_set[-1]
-            except IndexError:
-                n = 0
+    def generate_card(self):
+        """Generates a random card. 
 
-            # Filters the last card or the array to avoid having the same number twice in a row
-            filtered_card_range = list(filter(lambda exc: exc != n, card_range))
-
-            # Populates `self.card_set` with 7 new cards
-            card = random.choice(filtered_card_range)
-            self.card_set.append(card)
+        Returns:
+            A card as an integer.
+        """
+        return random.randint(1, 13)
 
         # In case of wanting to cheat, uncomment that below
         # print(self.card_set)
     
-    def calculate_points(self, choice):
+    def populate_cards_set(self, current_card):
+        """Populates the cards list for the game.
 
+        Args: 
+            current_card: Receives an integer as a card to store in the list.
+        
+            Verifies if the card was alredy in the list, and resets the list
+            when it has 13 cards, saving only the last one.
+        """
+        card = current_card
+
+        if len(self.card_set) < 13:
+            while card in self.card_set:
+                card = self.generate_card()
+            else:
+                self.card_set.append(card)
+        else:
+            last_card = self.get_last_card()
+            self.card_set.clear()
+            self.card_set.append(last_card)
+
+    def get_last_card(self):
+        """Returns the las card that was played. 
+
+        Returns:
+            A card as an integer.
+        """
+        index = len(self.card_set) - 1
+        return self.card_set[index]
+    
+    def compute_points(self, result):
+        """Computes the score of the player. 
+
+        Returns:
+            An integer with the sum of the points.
+        """
         # Add points
-        if (choice == "h" and self.card_set[1] >= self.card_set[0]) or (choice == "l" and self.card_set[1] <= self.card_set[0]):
+        if result == 'add':
             self.points += 100
 
         # Subtract points
-        elif choice == "l" or choice == "h":
+        elif result == 'remove':
             self.points -= 75
-
-        # In case of a typo
-        else:
-            print(f"Sorry, '{choice}' is not an option. GAME OVER!")
-            quit()
