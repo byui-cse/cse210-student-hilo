779035979f697beb1f1cbf18cec8d8a0520464e0
RyderGarache <gar21032@bui.edu>
2021-10-13 21:31:26 -0600

    hello

--- hilo/game/director.py
+++ hilo/game/director.py
@@ -85,3 +85,6 @@ class Dealer:
         if self.player.again == "n":
             print(f"Final score is {self.score}")
             self.keep_playing = False
+        
+    def can(self):
+        self.score=false
